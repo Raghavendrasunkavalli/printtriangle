9dca8c53b3c78f62b4f0ef012a8d758e6ade4928
Venkata Sai Raghavendra Rao Sunkavalli <sunkavalli2@usf.edu>
2021-01-28 15:41:56 -0500

    Final Version

--- Program.cs
+++ Program.cs
@@ -6,10 +6,23 @@ namespace printtriangle
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Q1 : Enter the number of rows for the traingle:");
+            int i,j,k;
+            Console.Write("Q1 : Enter the number of rows for the traingle:");
             string n = Console.ReadLine();
-            Console.Write(n);
-            Console.ReadLine();
+            int inputNumber = Convert.ToInt32(n);
+            for (i=1; i<=inputNumber;i++)
+            {
+                for (j=(inputNumber-1);j>=i;j--)
+                {
+                    Console.Write(" ");
+                }
+                for (k=1; k<= (2*i-1); k++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+                
         }
     }
 }
