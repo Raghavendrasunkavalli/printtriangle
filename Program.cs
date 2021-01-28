a69bb1153f9ca9da5aff9012e868e61cbaa2fe20
Venkata Sai Raghavendra Rao Sunkavalli <sunkavalli2@usf.edu>
2021-01-28 14:20:14 -0500

    Print triangle

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace printtriangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Q1 : Enter the number of rows for the traingle:");
+            string n = Console.ReadLine();
+            Console.Write(n);
+        }
+    }
+}
