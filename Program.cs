2b7f994a03e2761a401c572a585a412af786df76
Venkata Sai Raghavendra Rao Sunkavalli <sunkavalli2@usf.edu>
2021-02-01 08:57:37 -0500

    Final Version

--- Program.cs
+++ Program.cs
@@ -7,18 +7,18 @@ namespace printtriangle
         static void Main(string[] args)
         {
             int i,j,k;
-            Console.Write("Q1 : Enter the number of rows for the traingle:");
-            string n = Console.ReadLine();
-            int inputNumber = Convert.ToInt32(n);
-            for (i=1; i<=inputNumber;i++)
+            Console.Write("Q1 : Enter the number of rows for the traingle:");// User message to input number of rows in a triangle
+            string n = Console.ReadLine();// Reading the input from console.
+            int inputNumber = Convert.ToInt32(n);// COnverting string format to integer
+            for (i=1; i<=inputNumber;i++) // Initializing for loop to create number of rows in a triangle.
             {
-                for (j=(inputNumber-1);j>=i;j--)
+                for (j=(inputNumber-1);j>=i;j--) // Initializing for loop to create spaces before the * in a row
                 {
-                    Console.Write(" ");
+                    Console.Write(" "); // Printing white spaces on to the console.
                 }
-                for (k=1; k<= (2*i-1); k++)
+                for (k=1; k<= (2*i-1); k++) // Initializing for loop to print * on to the console.
                 {
-                    Console.Write("*");
+                    Console.Write("*"); // Printing * on to the console.
                 }
                 Console.WriteLine();
             }
